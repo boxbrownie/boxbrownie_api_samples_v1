8ccd688e9026ea7db410d1a91854ec3e9a15c47f
jetpackdigital <adam.hanna@jetpack.com.au>
2018-04-24 13:50:27 +1000

    Included XML sample

--- V1/boxbrownie_api_sample.php
+++ V1/boxbrownie_api_sample.php
@@ -1,4 +1,22 @@
-<?php  
+<?php 
+
+$xml = '
+	<job>
+	<jobTitle>Sample Job</jobTitle>
+	<input>
+		<files>
+			<fileURL>http://example.com/img1.jpg</fileURL>
+		</files>
+		<edits>
+			<edit>
+				<editName>Image Enhancement</editName>
+			</edit>
+		</edits>
+	</input>
+</job>
+';
+
+$token =  
 $url = 'https://api.boxbrownie.com/v1/job';                                                                 
 $headers = ['Content-Type: application/xml','Authorization: $token'];
 $ch = curl_init($url);		
@@ -7,4 +25,5 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);	
 $response = curl_exec($ch);		
+
 ?>			
